d80c9404b961f29f860bb3aba6ef56ac707c8050
John J Hritz <john-j-hritz@sbcglobal.net>
2018-10-24 15:53:19 -0700

    Script receives HTTP messages, extracts and forwards commands to serial.

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from time import sleep_ms
 import socket
 
 BOT_MODE = False                                    # Set true if the ESP8266 is controlling a bot.
+commandString = b'cmd='                             # String that precedes a command in the HTTP request
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]     # requests the IP address of the ESP 8266
 print('listening on', addr)
 
@@ -67,8 +68,14 @@ sleep_ms(20)
 led5.value(0)
 
 
-# Callback function for Interrupt tied to pressing Button 2
+
 def callback(p):
+    """
+    Callback function for Interrupt tied to pressing Button 2.
+    Activates Bot Mode, which allows the Nub060 to write to its serial port.  However, this disables REPL over serial.
+    :param p: The pin attached to the interrupt.  Passed automatically.
+    :return: None
+    """
     global BOT_MODE, uart
     if BOT_MODE:
         return
@@ -89,28 +96,61 @@ def callback(p):
 button2.irq(trigger=Pin.IRQ_FALLING, handler=callback)
 
 
-def motor_generic(request):
-    """ Writes the extracted command to the UART serial port """
-    cmd = request[10:11]
-    print(cmd)
-    uart.write(cmd)
+def send_command(command):
+    """
+    Writes the extracted command to the UART serial port.
+    :param command: The extracted message that will be sent to the Arduino
+    :return: None
+    """
+    uart.write(command)
+
 
 def extract_request(request_file):
-    request = ""  # String of the HTTP request
+    """
+    Extracts the line containing the GET request from the Raspberry Pi from the request file.
+    :param request_file: The file containing the complete HTTP request
+    :return: The line containing the HTTP GET request.
+    """
+    # String of the HTTP request.  Should be the first line of the file.
+    request = request_file.readline()
+    print(request)
 
-    # iterate through the file
-    with open(request_file) as f:
-        for line in f:
-            if request == "":
-                request = line
-            if not line or line == b'\r\n':
-                break
+    return request
 
 
+def extract_command(request_line):
+    """
+    Extracts the command from the HTTP GET request line.
+    :param request_line: The HTTP GET request line.
+    :return: The command to forward to the Arduino.
+    """
+    # Check if this is the GET request and we've received the command string
+    if b'GET' in request_line and commandString in request_line:
+
+        """
+        Find where the command string and HTTP stuff we don't care about start.
+        The command will be between those two.
+        """
+        cmd_index = request_line.find(commandString)
+        http_index = request_line.find(b' HTTP')
+        print("cmd_index: " + str(cmd_index))
+        print("http_index: " + str(http_index))
+
+        # All hail Slices. The command sits between the end of the command string and the start of the HTTP junk.
+        command = request_line[(cmd_index + len(commandString)):http_index]
+        print(command)
+
+    else:
+        print("Not a valid command line.  Sorry.")
+
+    return command
 
 
 def soc_request():
-    """ Receives HTTP requests incoming on Port 80 """
+    """
+    Receives and processes HTTP requests incoming on Port 80
+    :return: None
+    """
     while True:
         # accept the incoming request
         cl, clientaddr = s.accept()
@@ -118,19 +158,16 @@ def soc_request():
 
         # Write the HTTP request into a file so we can access it
         cl_file = cl.makefile('rwb', 0)
+        # Extract the HTTP GET request line from the message
+        request_line = extract_request(cl_file)
+        # Extract the command from the HTTP GET request line
+        command = extract_command(request_line)
+        # Send the command to the Arduino
+        send_command(command)
 
-
-
-
-        """
-        while True:
-            # read line from file
-            line = cl_file.readline()
-            if request == "":
-                request = line
-            if not line or line == b'\r\n':
-                break
-        """
-        motor_generic(request)
+        # Tell the client something so it will go away
         cl.send(b"OK")
+
+        # Close the file and socket to save memory
+        cl_file.close()
         cl.close()
